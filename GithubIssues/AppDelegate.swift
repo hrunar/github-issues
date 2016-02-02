69269eaa0f7393ea17dd0337875b3cb9bf4ddaa9
runsvend <runar.svendsen@finn.no>
2016-02-02 12:49:43 +0100

    Use network-based app

--- GithubIssues/AppDelegate.swift
+++ GithubIssues/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         seed(context)
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window?.rootViewController = coreDataApp(context) // app()
+        window?.rootViewController = app()
         window?.makeKeyAndVisible()
         return true
     }
